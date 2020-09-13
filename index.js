9ff717c330fed0164baa625949b5a614b2c33392
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-09-13 18:51:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const gifts = ["teddy bear", "drone", "doll"];
 
 function writeCards(name, giftType){
-  for(let i=0; i<name.length; i++){
-  let thankYou=console.log(`Thank you, ${name[i]}, for the wonderful ${giftType} gift!`)
+  let thank you=for(let i=0; i<name.length; i++){
+  console.log(`Thank you, ${name[i]}, for the wonderful ${giftType} gift!`)
   }
   return thankYou;
 }
