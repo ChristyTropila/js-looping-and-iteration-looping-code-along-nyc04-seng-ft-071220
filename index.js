873209ddd5fd19159cca372644a48273ea9de40d
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-09-13 18:58:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Code your solutions in this file
-const name=["Ada", "Brendan", "Ali"]
 
 function writeCards(name, giftType){
+  let thankYou=[]
   for(let i=0; i<name.length; i++){
-  console.log(`Thank you, ${name[i]}, for the wonderful ${giftType} gift!`)
+  console.log.push(`Thank you, ${name[i]}, for the wonderful ${giftType} gift!`)
   }
+  return thankYou;
 }
